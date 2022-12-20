21337e65122cb71b500095841a2fe5ddf3a55488
Chandresh <chandresh@bsquare.in>
2022-12-20 12:25:27 +0530

    test

---
+++ config/erpconst.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+        "attachments"=>[
+                "allow_extensions"=>"jpg,jpeg,png,gif,xls,xlsx,doc,docx,ppt,pptx,pdf,csv,txt,tiff,bmp,dwg",
+                "max_size"=>"2", 
+				"allow_view_file"=>"jpg,jpeg,png,gif,pdf,txt,bmp",
+         ],
+         "bank"=>[                
+                 "account_type"=>"SAVING ACCOUNT,CURRENT ACCOUNT,OD,OTHERS",
+         ],
+		 
+
+
+];
